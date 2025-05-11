8759a84309cd81038b005e1d26ffc62d2244d6bd
Chacha Nurholis <chachanurholis29@gmail.com>
2025-05-11 14:48:04 +0700

    improve(component): footer

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-    <section class="">
-      <Header />
-    </section>
+  <div class="flex flex-col min-h-screen">
+    <Header />
 
-    <div class="container mx-auto px-4 py-4">
+    <main class="container mx-auto px-4 py-4 flex-grow">
       <slot />
-    </div>
+    </main>
 
-    <section>
-      <Footer />
-    </section>
+    <Footer />
   </div>
 </template>
